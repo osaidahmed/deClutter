140d569d0672fa72c12f10198afee89d628db4a1
osaidahmed <omarsaidahmed123@gmail.com>
2024-12-30 23:00:23 -0500

    Cleared NextJS setup code

---
+++ app/list.tsx
@@ -0,0 +1,10 @@
+/*
+Manage a list of websites that the user appends to the list. 
+Dynamic array of items where each item is an object storing website information
+*/
+
+const list = () => {
+  return (
+  <div>list</div>
+);
+};
