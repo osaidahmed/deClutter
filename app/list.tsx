f19c8acd3348474bacd2edd0d7eeefcf5dea9dae
osaidahmed <omarsaidahmed123@gmail.com>
2024-12-30 23:55:26 -0500

    list.tsx - added input capablities and a button

--- app/list.tsx
+++ app/list.tsx
@@ -3,8 +3,37 @@ Manage a list of websites that the user appends to the list.
 Dynamic array of items where each item is an object storing website information
 */
 
-const list = () => {
-  return (
-  <div>list</div>
-);
+"use client";
+import { useState } from "react";
+
+interface Website {
+    name: string;
+    url: string;
+    tag: string;
+}
+
+const List = () => {
+    const [websites, setWebsites] = useState<Website[]>([]);
+    const [name, setName] = useState<string>("");
+    const [url, setUrl] = useState<string>("");
+    const [tag, setTag] = useState<string>("");
+
+    const addWebsite = () => { 
+        setWebsites([...websites, { name, url, tag }]);
+        setName("");
+        setUrl("");
+        setTag("");
+    };
+    return (
+        <div>
+            <h2 className = "text-xl">Lists</h2>
+            <input type = "text" value = {name} onChange = {(e) => setName(e.target.value)} placeholder="Name"/>
+            <input type = "text" value = {url} onChange = {(e) => setUrl(e.target.value)} placeholder="URL"/>
+            <input type = "text" value = {tag} onChange = {(e) => setTag(e.target.value)} placeholder="Tag"/>
+
+            <button onClick = {addWebsite}> Add Website</button>
+        </div>
+    );
 };
+
+export default List;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import List from "./list";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center h-screen">
-      <h1>Home</h1>
+      <h1 className = "text-2xl">Home</h1>
+
+      <List />
     </div>
   );
 }
